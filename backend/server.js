1a712c1ff6c0f1424ea62534394a8af9dd09a939
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2025-12-01 13:35:57 -0700

    new push

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
