63d887d84ebb32ee48d201540a809708347e3bb6
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2026-01-10 15:49:05 -0700

    prepared for the deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,27 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import categoriesRoutes from "./routes/categoryRoutes.js";
 import choicesRoutes from "./routes/choiceRoutes.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/categories", categoriesRoutes);
 app.use("/api/choices", choicesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
